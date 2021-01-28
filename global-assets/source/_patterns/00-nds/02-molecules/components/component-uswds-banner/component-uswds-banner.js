aa5b0ac3ed230b82d485988d512644922c973148
Jake Caccamo <caccamo_jacob@bah.com>
2021-01-28 16:28:48 -0500

    Refactoring USWDS Banner to eliminate dependency, removing matchMedia

---
+++ global-assets/source/_patterns/00-nds/02-molecules/components/component-uswds-banner/component-uswds-banner.js
@@ -0,0 +1,20 @@
+// Part of NDS Lite
+
+document.addEventListener("DOMContentLoaded", (e) => {
+    initComponentUSWDSBanner();
+});
+
+
+// initComponentUSWDSBanner - Toggles the USWDS Banner Component open and closed.
+function initComponentUSWDSBanner() {
+    document.querySelector('#uswds-banner-toggle').addEventListener("click", function(e) {
+        if (document.getElementById("uswds-banner-toggle").getAttribute('aria-expanded') == 'true') {
+            document.getElementById("uswds-banner-toggle").setAttribute('aria-expanded','false');
+            document.getElementById("uswds-banner-content").style.display = 'none';
+        }
+        else {
+            document.getElementById("uswds-banner-toggle").setAttribute('aria-expanded','true');
+            document.getElementById("uswds-banner-content").style.display = 'block';
+        }
+    });
+}
