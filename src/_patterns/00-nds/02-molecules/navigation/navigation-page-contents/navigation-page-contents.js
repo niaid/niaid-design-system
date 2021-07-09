25fbc3461fdec0257137d915cd337543a133fba4
Jacob Caccamo <caccamojr@niaid.nih.gov>
2021-07-09 16:50:50 -0400

    Adding Stickybits

--- src/_patterns/00-nds/02-molecules/navigation/navigation-page-contents/navigation-page-contents.js
+++ src/_patterns/00-nds/02-molecules/navigation/navigation-page-contents/navigation-page-contents.js
@@ -9,4 +9,30 @@ $(document).ready(function() {
         $(headings[i]).nextUntil("h2").addBack().wrapAll('<div id="' + headingID + '" class="component-nds component--scrollspy-section scrollspy section">');
     }
     $('.scrollspy').scrollSpy();
+
+    if ($(".navigation--page-contents").length) {
+        var mql = window.matchMedia('all and (min-width: 992px)');
+        var wWidth = $(window).width();
+
+        $('.layouts--main').each(function() {
+            if (mql.matches) {
+                stickybits($(this).find('.navigation--page-contents'), { stickyBitStickyOffset: 32 });
+            }
+        });
+
+        $(window).on('resize', function() {
+            if (wWidth != $(this).width()) {
+                wWidth = $(this).width();
+                $('.fixed-right').each(function() {
+                    if (mql.matches) {
+                        stickybits($(this).find('.navigation--page-contents'), { stickyBitStickyOffset: 32 });
+                    }
+                    else {
+                        stickybits($(this).find('.navigation--page-contents')).cleanup();
+                        $(this).find('.navigation--page-contents').css('position', 'relative');
+                    }
+                });
+            }
+        });
+    }
 });
