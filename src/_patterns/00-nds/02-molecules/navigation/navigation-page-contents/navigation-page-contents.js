e741dfabd0ede8eb9c34b820d1b79a7d40c60c56
Jacob Caccamo <caccamojr@niaid.nih.gov>
2021-07-26 15:23:36 -0400

    Fixing Spacing for Local Nav and Page Contents

--- src/_patterns/00-nds/02-molecules/navigation/navigation-page-contents/navigation-page-contents.js
+++ src/_patterns/00-nds/02-molecules/navigation/navigation-page-contents/navigation-page-contents.js
@@ -10,12 +10,12 @@ var moduleNDS_pageContents = (function() {
     // initPageContents - Initializes the Page Contents functionality.
     function initPageContents() {
         let headings = $('.layouts--body').children('h2');
-        let $tocList = $('.navigation--page-contents__list');
+        let $tocList = $('.navigation--page-contents__wrapper__list');
         for (let i = 0; i < headings.length; i++) {
             let headingID = 'scrollspy-section-' + $(headings[i]).text();
             headingID = headingID.replace(/\//g, '-');
             headingID = headingID.replace(/\s+/g, '-').replace('&', 'and').toLowerCase();
-            $tocList.append('<li class="navigation--page-contents__list__item"><a href="#' + headingID + '">' + $(headings[i]).text() + '</a></li>');
+            $tocList.append('<li class="navigation--page-contents__wrapper__list__item"><a href="#' + headingID + '">' + $(headings[i]).text() + '</a></li>');
             $(headings[i]).nextUntil("h2").addBack().wrapAll('<div id="' + headingID + '" class="component-nds component--scrollspy-section scrollspy section">');
         }
         $('.scrollspy').scrollSpy();
@@ -23,7 +23,7 @@ var moduleNDS_pageContents = (function() {
 
         $('.navigation--page-contents').each(function() {
             if ($(this).attr('data-sticky') == "true") { 
-                stickyElement(".navigation--page-contents", 32, "all and (min-width: 992px)");
+                stickyElement(".navigation--page-contents", 0, "all and (min-width: 992px)");
             }
         });
     }
