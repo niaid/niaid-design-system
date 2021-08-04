e4382cd0f4da6cce1948173401c1ef92ba7c8e4a
Jacob Caccamo <caccamojr@niaid.nih.gov>
2021-08-04 11:18:07 -0400

    Fixing Data Attributes, Adding New Format Analytics Function, Adding Local Nav and Page Contents

--- src/_patterns/00-nds/02-molecules/navigation/navigation-page-contents/navigation-page-contents.js
+++ src/_patterns/00-nds/02-molecules/navigation/navigation-page-contents/navigation-page-contents.js
@@ -13,9 +13,10 @@ var moduleNDS_pageContents = (function() {
         let $tocList = $('.navigation--page-contents__wrapper__list');
         for (let i = 0; i < headings.length; i++) {
             let headingID = 'scrollspy-section-' + $(headings[i]).text();
+            let dataNavValue = moduleNDS_analytics.formatAttributeValue(headings[i]);
             headingID = headingID.replace(/\//g, '-');
             headingID = headingID.replace(/\s+/g, '-').replace('&', 'and').toLowerCase();
-            $tocList.append('<li class="navigation--page-contents__wrapper__list__item"><a href="#' + headingID + '">' + $(headings[i]).text() + '</a></li>');
+            $tocList.append('<li class="navigation--page-contents__wrapper__list__item"><a href="#' + headingID + '" data-nav="nav-right-' + dataNavValue + '">' + $(headings[i]).text() + '</a></li>');
             $(headings[i]).nextUntil("h2").addBack().wrapAll('<div id="' + headingID + '" class="component-nds component--scrollspy-section scrollspy section">');
         }
         $('.scrollspy').scrollSpy();
