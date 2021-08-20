487c53904f65ce3c6621227e1700c078d2e0de5d
Jacob Caccamo <caccamojr@niaid.nih.gov>
2021-08-20 17:08:47 -0400

    Fixing Twig Filters

--- alter-twig.php
+++ alter-twig.php
@@ -5,30 +5,17 @@
  * @param $config - Config of `@basalt/twig-renderer`
  */
 function addCustomExtension(\Twig_Environment &$env, $config) {
+    $env->addFunction(new Twig_SimpleFunction('link', function ($title, $url, $attributes) {
+      if (isset($attributes) && isset($attributes['class'])) {
+        $classes = join(' ', $attributes['class']);
+        return '<a href="' . $url . '" class="' . $classes . '">' . $title . '</a>';
+      }
+      else {
+        return '<a href="' . $url . '">' . $title . '</a>';
+      }
+    }, array('is_safe' => array('html'))));
 
-  /**
-   * @example `<h1>Hello {{ customTwigFunctionThatSaysWorld() }}!</h1>` => `<h1>Hello Custom World</h1>`
-   */
-//  $env->addFunction(new \Twig_SimpleFunction('customTwigFunctionThatSaysWorld', function () {
-//    return 'Custom World';
-//  }));
-
-  /*
-   * Reverse a string
-   * @param string $theString
-   * @example `<p>{{ reverse('abc') }}</p>` => `<p>cba</p>`
-   */
-//  $env->addFunction(new \Twig_SimpleFunction('reverse', function ($theString) {
-//    return strrev($theString);
-//  }));
-
-
-//  $env->addExtension(new \My\CustomExtension());
-
-//  `{{ foo }}` => `bar`
-//  $env->addGlobal('foo', 'bar');
-
-  // example of enabling the Twig debug mode extension (ex. {{ dump(my_variable) }} to check out the template's available data) -- comment out to disable
-  // $env->addExtension(new \Twig_Extension_Debug());
-
+    $env->addFilter(new Twig_SimpleFilter('render', function ($string) {
+      return $string;
+    }));
 }
