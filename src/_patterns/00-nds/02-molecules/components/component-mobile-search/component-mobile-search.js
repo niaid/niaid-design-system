e40ad337abe63527db99d3ebcb15d8513f94f51d
Jacob Caccamo <caccamojr@niaid.nih.gov>
2021-10-05 17:15:57 -0400

    Updating Mobile Search

--- src/_patterns/00-nds/02-molecules/components/component-mobile-search/component-mobile-search.js
+++ src/_patterns/00-nds/02-molecules/components/component-mobile-search/component-mobile-search.js
@@ -7,27 +7,29 @@ var moduleNDS_mobileSearch = (function() {
     /* =================== PRIVATE METHODS ================= */
     // initMobileSearch - Initializes the mobile search functionality.
     function initMobileSearch() {
-        document.querySelector('#component--mobile-search--open').addEventListener("click", function(e) {
-            openMobileSearch();
-        });
+        if ($('.component--mobile-search').length > 0) {
+            document.querySelector('#component--mobile-search--open').addEventListener("click", function(e) {
+                openMobileSearch();
+            });
 
-        document.querySelector('#component--mobile-search--close').addEventListener("click", function(e) {
-            closeMobileSearch();
-        });
-
-        window.addEventListener('resize', function() {
-            if (viewportIsMobile && viewportWidthIsDifferent) {
+            document.querySelector('#component--mobile-search--close').addEventListener("click", function(e) {
                 closeMobileSearch();
-            }
-        });
+            });
 
-        // 508 Compliance Focus Helpers
-        jQuery('.skip-to--front').on("focus", function(e) {
-            jQuery('.component--mobile-search__wrapper__search-form').find('input.input--text').focus();
-        });
-        jQuery('.skip-to--back').on("focus", function(e) {
-            jQuery('#component--mobile-search--close').focus();
-        });
+            window.addEventListener('resize', function() {
+                if (viewportIsMobile && viewportWidthIsDifferent) {
+                    closeMobileSearch();
+                }
+            });
+
+            // 508 Compliance Focus Helpers
+            jQuery('.skip-to--search').on("focus", function(e) {
+                jQuery('.component--mobile-search__wrapper__search-form').find('input.input--text').focus();
+            });
+            jQuery('.skip-to--close').on("focus", function(e) {
+                jQuery('#component--mobile-search--close').focus();
+            });
+        }
     }
 
     /* =================== PUBLIC METHODS ================== */
