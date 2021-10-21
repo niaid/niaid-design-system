4076d0014c46597837176e41b027ed6c14d9ff26
Jacob Caccamo <caccamojr@niaid.nih.gov>
2021-10-21 13:59:38 -0400

    Adding Keyboard Fix for Facets, and Mobile Search Tab Fix

--- src/_patterns/00-nds/01-atoms/input/_input/input-nds.js
+++ src/_patterns/00-nds/01-atoms/input/_input/input-nds.js
@@ -4,13 +4,23 @@ var moduleNDS_input = (function() {
     /* =================== PRIVATE METHODS ================= */
     // enableKeyboardAccessibility - Functionality to support keyboard accessibility on radio and checkbox inputs.
     function enableKeyboardAccessibility() {
-        let inputElements = document.querySelectorAll('.input--radio, .input--checkbox');
+        let inputElements = document.querySelectorAll('.input--radio, .input--checkbox, .facet-item');
         for (let i = 0; i < inputElements.length; i++) {
             inputElements[i].addEventListener('keydown', function(e) {
                 if (e.code === "Enter" || e.keyCode === "13" || e.code === "Space" || e.keyCode === "23") {
                     e.target.querySelector('input').click();
                 }
             });
+        }
+
+        let facetItems = document.querySelectorAll('.facet-item');
+        for (let j = 0; j < facetItems.length; j++) {
+            if (!facetItems[j].hasAttribute('tabindex')) {
+                facetItems[j].setAttribute('tabindex', '0');
+            }
+            if (!facetItems[j].querySelector('input').hasAttribute('tabindex')) {
+                facetItems[j].querySelector('input').setAttribute('tabindex', '-1');
+            }
         }
     }
 
